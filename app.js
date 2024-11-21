4b38e7bf52ed5113a2481a1826a6a784800635bb
YehielSiri <yehiels14@gmail.com>
2024-11-21 13:28:23 +0200

    a working converter - version 2.1

--- app.js
+++ app.js
@@ -11,37 +11,40 @@ app.get(`${api}/hebrewdate`, (req, res) =>{
     var hebdate = gregdate.toLocaleString('he-u-ca-hebrew');
     
     // Version 1.0 - using JS libraries:
-    //res.send('Gregorian date: '+ req.query.date + '\nHebrew calendar date: ' + hebdate);
+   // res.send('Gregorian date: '+ req.query.date + '\nHebrew calendar date: ' + hebdate);
     /*Output Example:
     *    Gregorian date: 2024-11-12
     *    Hebrew calendar date: 11 בחשוון 5785, 2:00:00
     */
 
     // Version 1.1 - using YaelGroup sevice:
-    // using fetch
-    // fetch("https://www.hebcal.com/converter?cfg=json&date="+req.query.date+"&g2h=1&strict=1")
-    // .then((response)=>{
-    //     if(!response.ok)
-    //     throw new Error("Network response is not ok");
-    //     return response.json;
-    // }).then((data)=>console.log(data))
-    // .catch((error)=>console.error("Error in Fetch: ",error))
-    //using XMLHttpRequest
-    var xhr=new XMLHttpRequest();
-    xhr.open("GET","https://www.hebcal.com/converter?cfg=json&date="+req.query.date+"&g2h=1&strict=1",true);
-    xhr.onload = function(){
-        if(xhr.status>=200 && xhr.status<300){
-            console.log(JSON.parse(khr.response))
+    // 1.1.0 using fetch
+    let fetchRes = fetch("https://www.hebcal.com/converter?cfg=json&date="+req.query.date+"&g2h=1&strict=1")
+     fetchRes.then(response=>response.json()).then(d=>{res.send(d)})
+        //(response)=>{
+         //if(!response.ok)
+         //throw new Error("Network response is not ok");
+       //  return response.json;
+     //}).then((data)=>res.send(data))
+     //.catch((error)=>console.error("Error in Fetch: ",error))
 
-        } else{
-            console.error("Error message: ",xhr.statusText);
-        }
+    // 1.1.1 using XMLHttpRequest
+    // var xhr=new XMLHttpRequest();
+    // xhr.open("GET","https://www.hebcal.com/converter?cfg=json&date="+req.query.date+"&g2h=1&strict=1",true);
+    // xhr.onload = function(){
+    //     if(xhr.status>=200 && xhr.status<300){
+    //         console.log(JSON.parse(khr.response))
 
-    };
-    xhr.onerror = function(){
-        console.error("Request failed");
-    };
-    xhr.send();
+    //     } else{
+    //         console.error("Error message: ",xhr.statusText);
+    //     }
+
+    // };
+    // xhr.onerror = function(){
+    //     console.error("Request failed");
+    // };
+    // xhr.send();
+
 })
 
 app.listen(3000, ()=>{
